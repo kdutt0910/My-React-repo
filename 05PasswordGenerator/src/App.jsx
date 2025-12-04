dcdd8e5e5aab2508ddd3ae546cf75e84e103711f
kdutt0910 <kdutt000@gmail.com>
2025-12-04 10:04:10 +0000

    Password Generator

--- 05PasswordGenerator/src/App.jsx
+++ 05PasswordGenerator/src/App.jsx
@@ -1,10 +1,37 @@
-import { useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 
 
 function App() {
   const [length, setLength] = useState(8);
   const [includeNumbers, setIncludeNumbers] = useState(false);
   const [includeCharacters, setIncludeCharacters] = useState(false);
+  const [password, setPassword] = useState("");
+
+  const copyTextRef = useRef(null)
+
+  const passwordGenerator = useCallback(()=>{
+      let pass = ""
+      let str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+
+      if(includeNumbers){ str += '1234567890' }
+      if(includeCharacters){ str += '~!@#$%^&*-_+=[]{}`' }
+
+      for(let i = 1; i <= length; i++){
+        let char = Math.floor(Math.random() * str.length + 1)
+        pass += str.charAt(char);
+      }
+      setPassword(pass)   
+  },[length,includeCharacters,includeNumbers,setPassword])
+  const copyPasswordToClip = useCallback(() => {
+    copyTextRef.current?.select();
+    copyTextRef.current?.setSelectionRange(0,100)
+    window.navigator.clipboard.writeText(password)
+  },[password])
+
+  useEffect(() => {
+    passwordGenerator()
+  },[length,includeCharacters,includeNumbers,passwordGenerator])
+  
 
   return (
     <div className="h-screen flex flex-col justify-center items-center bg-gray-900 text-white gap-8">
@@ -14,10 +41,13 @@ function App() {
         <input
           type="text"
           readOnly
-          value="GeneratedPassword123!"
+          value={password}
+          ref={copyTextRef}
           className="bg-gray-700 text-white px-3 py-2 rounded w-full"
         />
-        <button className="bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded">
+        <button 
+        onClick={copyPasswordToClip}
+        className="bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded">
           Generate
         </button>
       </div>
@@ -27,11 +57,11 @@ function App() {
           <label htmlFor="length" className="whitespace-nowrap">Length ({length})</label>
         </div>
         <div className="flex items-center gap-2">
-          <input type="checkbox" name="Numeric" className="checkbox" checked={includeNumbers} onChange={(e) => setIncludeNumbers(e.target.checked)} />
+          <input type="checkbox" name="Numeric" className="checkbox" defaultChecked={includeNumbers} onChange={() => setIncludeNumbers((prev) => !prev)} />
           <label htmlFor="Numeric">Include Numbers</label>
         </div>
         <div className="flex items-center gap-2">
-          <input type="checkbox" name="Character" className="checkbox" checked={includeCharacters} onChange={(e) => setIncludeCharacters(e.target.checked)} />
+          <input type="checkbox" name="Character" className="checkbox" defaultChecked={includeCharacters} onChange={() => setIncludeCharacters((prev) => !prev)} />
           <label htmlFor="Character">Include Special Characters</label>
         </div>
       </div>
